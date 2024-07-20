d728b66221d273d57746dabb52bee550c6fcab19
Ahmed <ahmedelsharawy117@gmail.com>
2024-07-20 13:43:18 +0300

    some changes

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -5,8 +5,8 @@ require("config.lazy")
 require("mappings")
 
 function TransColor()
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 TransColor()
