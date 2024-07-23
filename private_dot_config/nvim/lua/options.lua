e7ed45cded09738391fc977e02dd59bc4549d065
Ahmed <ahmedelsharawy117@gmail.com>
2024-07-23 23:19:27 +0300

    update(nvim): add colorsheme and set a new option

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -5,15 +5,16 @@ vim.g.maplocalleader = "\\"
 vim.wo.relativenumber = true
 opt.number = true
 opt.swapfile = false
-opt.expandtab = true          -- Use spaces instead of tabs
-opt.tabstop = 2               -- Number of spaces a tab counts for
-opt.shiftwidth = 2            -- Number of spaces used for each step of (auto)indent
-opt.softtabstop = 2           -- Number of spaces a tab displays as
+opt.expandtab = true -- Use spaces instead of tabs
+opt.tabstop = 2 -- Number of spaces a tab counts for
+opt.shiftwidth = 2 -- Number of spaces used for each step of (auto)indent
+opt.softtabstop = 2 -- Number of spaces a tab displays as
 opt.fillchars = { eob = " " } -- Remove the ~ line
 opt.undofile = true
 -- Copy to system clipboard
 opt.clipboard = "unnamedplus"
-vim.o.signcolumn = "no" -- Remove the W char beside the line number
+-- vim.o.signcolumn = "no" -- Remove the W char beside the line number
+vim.o.signcolumn = "yes"
 vim.o.background = "dark"
 opt.termguicolors = true
 opt.scrolloff = 8
