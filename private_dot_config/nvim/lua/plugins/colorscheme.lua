30edcaa4be6dc859b1e3a345f22853cc305cdeea
Ahmed <ahmedelsharawy117@gmail.com>
2024-11-01 16:29:00 +0200

    nvim: gruvbox theme

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,22 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "auto",
-        bufferline = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-      })
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "auto",
+				bufferline = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+			})
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = true,
+		opts = ...,
+	},
 }
