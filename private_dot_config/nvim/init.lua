4afea6201b1a8c162aad856e1059a1f6bcbcd7d9
Ahmed <ahmedelsharawy117@gmail.com>
2024-11-01 16:29:16 +0200

    .

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -5,6 +5,7 @@ require("config.lazy")
 require("mappings")
 
 vim.cmd.colorscheme("catppuccin-mocha")
+-- vim.cmd.colorscheme("gruvbox")
 -- make neovim transparent
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
