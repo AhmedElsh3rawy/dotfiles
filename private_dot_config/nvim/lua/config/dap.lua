808d02509778520ab879e1e6428b89a1c8e8a44e
Ahmed <ahmedelsharawy117@gmail.com>
2024-11-16 22:13:05 +0200

    feat(nvim): debugging nodejs

---
+++ private_dot_config/nvim/lua/config/dap.lua
@@ -0,0 +1,32 @@
+local dap = require("dap")
+
+require("dap").adapters["pwa-node"] = {
+  type = "server",
+  host = "localhost",
+  port = "${port}",
+  executable = {
+    command = "node",
+    -- 💀 Make sure to update this path to point to your installation
+    args = {
+      os.getenv("HOME") .. "/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+      "${port}",
+    },
+  },
+}
+
+for _, language in ipairs({ "typescript", "javascript" }) do
+  dap.configurations[language] = {
+    {
+      type = "pwa-node",
+      request = "launch",
+      name = "Launch file",
+      program = "${file}",
+      cwd = "${workspaceFolder}",
+      runtimeExecutable = "node",
+      stopOnEntry = true,
+      onExit = function()
+        print("Debug session exited")
+      end,
+    },
+  }
+end
