b7fe92cc01059155accd5b2884a7df7bba8dcdb3
Ahmed <ahmedelsharawy117@gmail.com>
2025-01-02 00:03:16 +0200

    init

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "c", "cpp", "lua", "javascript", "typescript", "tsx", "nix", "rust", "go", "python" },
-      highlight = { enable = true, use_languagetree = true },
-      indent = { enable = true },
-    })
-  end,
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = { "c", "cpp", "lua", "javascript", "typescript", "tsx", "nix", "rust", "go", "python" },
+            highlight = { enable = true, use_languagetree = true },
+            indent = { enable = true },
+        })
+    end,
 }
