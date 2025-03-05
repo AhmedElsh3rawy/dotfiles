13a748c4fcf78b70cbee2a5a4c166150a7aec627
Ahmed <ahmedelsharawy117@gmail.com>
2025-03-05 23:46:12 +0200

    feat(nvim): add gruvbox theme

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -5,7 +5,7 @@ require("config.lazy")
 require("mappings")
 
 vim.cmd.colorscheme("catppuccin-mocha")
--- vim.cmd.colorscheme("gruvbox")
+-- vim.cmd.colorscheme("gruvbox-material")
 -- make neovim transparent
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,25 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "auto",
-				bufferline = true,
-				gitsigns = true,
-				nvimtree = true,
-				treesitter = true,
-			})
-		end,
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		config = true,
-		opts = ...,
-	},
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "auto",
+                bufferline = true,
+                gitsigns = true,
+                nvimtree = true,
+                treesitter = true,
+            })
+        end,
+    },
+    {
+        "sainnhe/gruvbox-material",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.g.gruvbox_material_enable_italic = true
+            vim.g.gruvbox_material_background = "hard"
+        end,
+    },
 }
