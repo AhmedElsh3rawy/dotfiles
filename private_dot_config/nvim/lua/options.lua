29f32e77ed37a0dd8733a4d1cc9024721a9c1a55
Ahmed <ahmedelsharawy117@gmail.com>
2025-03-05 23:47:05 +0200

    style(nvim): formatting

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -1,25 +1,29 @@
 local opt = vim.opt
+local O = vim.o
+local G = vim.g
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-vim.wo.relativenumber = true
+G.mapleader = " "
+G.maplocalleader = "\\"
+G.loaded_netrw = 0
+G.loaded_netrwPlugin = 0
+
 opt.number = true
 opt.swapfile = false
-opt.expandtab = true          -- Use spaces instead of tabs
-opt.tabstop = 4               -- Number of spaces a tab counts for
-opt.shiftwidth = 4            -- Number of spaces used for each step of (auto)indent
-opt.softtabstop = 4           -- Number of spaces a tab displays as
+opt.expandtab = true -- Use spaces instead of tabs
+opt.tabstop = 4 -- Number of spaces a tab counts for
+opt.shiftwidth = 4 -- Number of spaces used for each step of (auto)indent
+opt.softtabstop = 4 -- Number of spaces a tab displays as
 opt.fillchars = { eob = " " } -- Remove the ~ line
 opt.undofile = true
 opt.clipboard = "unnamedplus" -- Copy to system clipboard
-
-vim.o.signcolumn = "yes"
-vim.o.background = "dark"
 opt.termguicolors = true
 opt.scrolloff = 8
 opt.cursorline = true
+
+O.signcolumn = "yes"
+O.background = "dark"
+-- O.spell = true
+-- O.spelllang = "en"
+
 vim.api.nvim_buf_set_option(0, "syntax", "off")
-vim.g.loaded_netrw = 0
-vim.g.loaded_netrwPlugin = 0
-vim.o.spell = true
-vim.o.spelllang = "en"
+vim.wo.relativenumber = true
