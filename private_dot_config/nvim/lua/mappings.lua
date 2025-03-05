79d062c94720c1262cacc6d54826f6116e87020d
Ahmed <ahmedelsharawy117@gmail.com>
2025-03-05 23:46:48 +0200

    feat(nvim): harpoon mappings

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -1,21 +1,23 @@
-local builtin = require("telescope.builtin")
 local map = vim.keymap.set
+
+local builtin = require("telescope.builtin")
+-- Telescope
 map("n", "<leader>ff", builtin.find_files, { desc = "Find file" })
 map("n", "<leader>fw", builtin.live_grep, { desc = "Find word" })
 map("n", "<leader>fb", builtin.buffers, { desc = "Find buffer" })
 map("n", "<leader>fh", builtin.help_tags, { desc = "Find help" })
+map("n", " <leader>fm", "<CMD>Telescope harpoon marks<CR>", { desc = "Find marked file" })
+-- nvim-tree
 map("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Nvim tree" })
+-- close buffer
 map("n", "<leader>bc", ":BufferLinePickClose<CR>", { desc = "Pick buffer to close" })
+-- jump normal mode
 map("i", "jn", "<Esc>", {})
 -- Navigate vim panes better
 map("n", "<C-h>", "<C-w>h", { noremap = true, silent = true, desc = "switch window left" })
 map("n", "<C-l>", "<C-w>l", { noremap = true, silent = true, desc = "switch window right" })
 map("n", "<C-j>", "<C-w>j", { noremap = true, silent = true, desc = "switch window down" })
 map("n", "<C-k>", "<C-w>k", { noremap = true, silent = true, desc = "switch window up" })
--- map("n", "<S-k>", ":wincmd k<CR>")
--- map("n", "<S-j>", ":wincmd j<CR>")
--- map("n", "<S-h>", ":wincmd h<CR>")
--- map("n", "<S-l>", ":wincmd l<CR>")
 -- Navigate buffers
 map("n", "<Tab>", "<CMD>BufferLineCycleWindowlessNext<CR>", { noremap = true, silent = true })
 map("n", "<S-Tab>", "<CMD>BufferLineCycleWindowlessPrev<CR>", { noremap = true, silent = true })
@@ -24,3 +26,8 @@ map("n", "<leader>x", ":bd<CR>", { noremap = true, silent = true, desc = "Close 
 -- for debugging
 map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Add breakpoint at line" })
 map("n", "<leader>dr", "<cmd> DapContinue <CR>", { desc = "Start or continue the debugger" })
+-- harpoon
+map("n", "hm", require("harpoon.ui").toggle_quick_menu, { desc = "Harpoon menu" })
+map("n", "hx", require("harpoon.mark").add_file, { desc = "Harpoon add file" })
+map("n", "hn", require("harpoon.ui").nav_next, { desc = "Harpoon next" })
+map("n", "hp", require("harpoon.ui").nav_prev, { desc = "Harpoon previous" })
