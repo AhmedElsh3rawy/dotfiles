1602c060509df3f0aec844a4f1efae8e0dc2558a
Ahmed <ahmedelsharawy117@gmail.com>
2025-03-18 16:32:47 +0200

    chore(nvim): use moonfly colorscheme and transparant background

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -4,8 +4,9 @@ require("options")
 require("config.lazy")
 require("mappings")
 
-vim.cmd.colorscheme("catppuccin-mocha")
--- vim.cmd.colorscheme("gruvbox-material")
+-- vim.cmd.colorscheme("catppuccin-mocha")
+vim.cmd.colorscheme("moonfly")
+
 -- make neovim transparent
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
