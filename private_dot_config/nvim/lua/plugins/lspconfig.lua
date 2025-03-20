1de366bb9d00a4c3514220a0020f76ef952a8486
Ahmed <ahmedelsharawy117@gmail.com>
2025-03-20 02:13:12 +0200

    style(nvim): formatting

--- private_dot_config/nvim/lua/plugins/lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,83 +1,83 @@
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-  },
-  -- lazy = false,
-  config = function()
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
-    local lspconfig = require('lspconfig')
-    local util = require('lspconfig/util')
-    -- local capabilities = vim.lsp.protocol.make_client.capabilities()
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+    },
+    -- lazy = false,
+    config = function()
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        local lspconfig = require('lspconfig')
+        local util = require('lspconfig/util')
+        -- local capabilities = vim.lsp.protocol.make_client.capabilities()
 
-    local on_attach = function(_, bufnr)
-      local bufmap = function(keys, func)
-        local opts = { buffer = bufnr, noremap = true, silent = true }
-        vim.keymap.set('n', keys, func, opts)
-      end
+        local on_attach = function(_, bufnr)
+            local bufmap = function(keys, func)
+                local opts = { buffer = bufnr, noremap = true, silent = true }
+                vim.keymap.set('n', keys, func, opts)
+            end
 
-      -- bufmap("<leader>r", vim.lsp.bur.rename)
-      bufmap('K', vim.lsp.buf.hover)
-      bufmap('gd', vim.lsp.buf.definition)
-      bufmap('gD', vim.lsp.buf.declaration)
-      bufmap('gr', vim.lsp.buf.references)
-      bufmap('<leader>ca', vim.lsp.buf.code_action)
-      bufmap('gi', vim.lsp.buf.implementation)
-      bufmap('<leader>D', vim.lsp.buf.type_definition)
-    end
+            -- bufmap("<leader>r", vim.lsp.bur.rename)
+            bufmap('K', vim.lsp.buf.hover)
+            bufmap('gd', vim.lsp.buf.definition)
+            bufmap('gD', vim.lsp.buf.declaration)
+            bufmap('gr', vim.lsp.buf.references)
+            bufmap('<leader>ca', vim.lsp.buf.code_action)
+            bufmap('gi', vim.lsp.buf.implementation)
+            bufmap('<leader>D', vim.lsp.buf.type_definition)
+        end
 
-    local servers = { 'bashls', 'lua_ls', 'pylsp', 'clangd', 'eslint', 'html' }
+        local servers = { 'bashls', 'lua_ls', 'pylsp', 'ruff', 'clangd', 'eslint', 'html' }
 
-    lspconfig.ts_ls.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-      init_options = {
-        preferences = {
-          disableSuggestions = true,
-          organizeImports = true,
-        },
-      },
-    })
+        lspconfig.ts_ls.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            init_options = {
+                preferences = {
+                    disableSuggestions = true,
+                    organizeImports = true,
+                },
+            },
+        })
 
-    lspconfig.rust_analyzer.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-      filetypes = { 'rust' },
-      root_dir = util.root_pattern('Cargo.toml'),
-      settings = {
-        ['rust-analyzer'] = {
-          cargo = {
-            allFeatures = true,
-          },
-        },
-      },
-    })
+        lspconfig.rust_analyzer.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            filetypes = { 'rust' },
+            root_dir = util.root_pattern('Cargo.toml'),
+            settings = {
+                ['rust-analyzer'] = {
+                    cargo = {
+                        allFeatures = true,
+                    },
+                },
+            },
+        })
 
-    lspconfig.gopls.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-      cmd = { 'gopls' },
-      filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
-      root_dir = util.root_pattern('go.work', 'go.mod', '.git'),
-      settings = {
-        gopls = {
-          completeUnimported = true,
-          usePlaceholders = true,
-          analyses = {
-            unusedparams = true,
-          },
-        },
-      },
-    })
+        lspconfig.gopls.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            cmd = { 'gopls' },
+            filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
+            root_dir = util.root_pattern('go.work', 'go.mod', '.git'),
+            settings = {
+                gopls = {
+                    completeUnimported = true,
+                    usePlaceholders = true,
+                    analyses = {
+                        unusedparams = true,
+                    },
+                },
+            },
+        })
 
-    for _, lsp in ipairs(servers) do
-      lspconfig[lsp].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-    end
+        for _, lsp in ipairs(servers) do
+            lspconfig[lsp].setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+            })
+        end
 
-    vim.lsp.set_log_level('debug')
-  end,
+        vim.lsp.set_log_level('debug')
+    end,
 }
