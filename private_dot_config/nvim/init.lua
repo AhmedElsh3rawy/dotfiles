f3da83d424f69f538349a5074be8e8a3fb06a752
Ahmed <ahmedelsharawy117@gmail.com>
2025-03-30 00:48:45 +0200

    chore(nvim): catppuccin baby

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -4,9 +4,8 @@ require('options')
 require('config.lazy')
 require('mappings')
 
--- vim.cmd.colorscheme("catppuccin-mocha")
-vim.cmd.colorscheme('moonfly')
+vim.cmd.colorscheme('catppuccin-mocha')
 
 -- make neovim transparent
-vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
