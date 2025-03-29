fab99874962db35b1b2d380bf8a1e25485349f07
Ahmed <ahmedelsharawy117@gmail.com>
2025-03-30 00:51:14 +0200

    chore(nvim): remove bufferline mappings

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -10,7 +10,6 @@ map('x', '<leader>p', [["_dP]])
 -- Telescope
 map('n', '<leader>ff', builtin.find_files, { desc = 'Find file' })
 map('n', '<leader>fw', builtin.live_grep, { desc = 'Find word' })
-map('n', '<leader>fb', builtin.buffers, { desc = 'Find buffer' })
 map('n', '<leader>fh', builtin.help_tags, { desc = 'Find help' })
 map('n', ' <leader>fm', '<CMD>Telescope harpoon marks<CR>', { desc = 'Find marked file' })
 -- nvim-tree
@@ -24,11 +23,6 @@ map('n', '<C-h>', '<C-w>h', { noremap = true, silent = true, desc = 'switch wind
 map('n', '<C-l>', '<C-w>l', { noremap = true, silent = true, desc = 'switch window right' })
 map('n', '<C-j>', '<C-w>j', { noremap = true, silent = true, desc = 'switch window down' })
 map('n', '<C-k>', '<C-w>k', { noremap = true, silent = true, desc = 'switch window up' })
--- Navigate buffers
-map('n', '<Tab>', '<CMD>BufferLineCycleWindowlessNext<CR>', { noremap = true, silent = true })
-map('n', '<S-Tab>', '<CMD>BufferLineCycleWindowlessPrev<CR>', { noremap = true, silent = true })
-map('n', '<C-Tab>', '<CMD>BufferLineCycleWindowlessToggle<CR>', { noremap = true, silent = true })
-map('n', '<leader>x', ':bd<CR>', { noremap = true, silent = true, desc = 'Close buffer' })
 -- for debugging
 map('n', '<leader>db', '<cmd> DapToggleBreakpoint <CR>', { desc = 'Add breakpoint at line' })
 map('n', '<leader>dr', '<cmd> DapContinue <CR>', { desc = 'Start or continue the debugger' })
