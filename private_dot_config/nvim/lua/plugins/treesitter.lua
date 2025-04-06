2a772548505176b23b7f28f152a6838135a45439
Ahmed <ahmedelsharawy117@gmail.com>
2025-04-06 17:50:40 +0200

    feat(nvim): treesitter option

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,11 @@ return {
   config = function()
     require('nvim-treesitter.configs').setup({
       ensure_installed = { 'c', 'cpp', 'lua', 'javascript', 'typescript', 'nix', 'rust', 'go', 'python' },
-      highlight = { enable = true, use_languagetree = true },
+      highlight = {
+        enable = true,
+        use_languagetree = true,
+        additional_vim_regex_highlighting = false,
+      },
       indent = { enable = true },
     })
   end,
