95d1e767fbaff6eaf81263ec3ea2211dc608300a
Ahmed <ahmedelsharawy117@gmail.com>
2025-05-11 22:13:15 +0300

    feat: formating for some langs

--- private_dot_config/nvim/lua/plugins/conform.lua
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -5,6 +5,8 @@ return {
             formatters_by_ft = {
                 html = { 'prettier' },
                 css = { 'prettier' },
+                json = { 'prettier' },
+                yaml = { 'prettier' },
                 lua = { 'stylua' },
                 python = { 'ruff_format', 'black' },
                 go = { 'gofmt', 'goimports', 'golines' },
@@ -14,6 +16,7 @@ return {
                 c = { 'clang_format' },
                 cpp = { 'clang_format' },
                 sql = { 'sql_formatter' },
+                php = { 'pint' },
             },
 
             format_on_save = {
