df480d9b10a5797136e7cb0843337bb831043f44
Ahmed <ahmedelsharawy117@gmail.com>
2025-05-11 22:13:38 +0300

    chore: use tokyonight theme

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -5,7 +5,8 @@ require('config.lazy')
 require('config.lsp')
 require('mappings')
 
-vim.cmd.colorscheme('catppuccin-mocha')
+-- vim.cmd.colorscheme('catppuccin-mocha')
+vim.cmd.colorscheme('tokyonight-night')
 
 -- make neovim transparent
 -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
