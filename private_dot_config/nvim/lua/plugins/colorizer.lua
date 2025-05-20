ddbb6a7db28acbb52dea02ca16c0deb2693252a5
Ahmed <ahmedelsharawy117@gmail.com>
2025-05-20 19:52:52 +0300

    chore: update colorizer options

--- private_dot_config/nvim/lua/plugins/colorizer.lua
+++ private_dot_config/nvim/lua/plugins/colorizer.lua
@@ -1,11 +1,42 @@
 return {
-    'NvChad/nvim-colorizer.lua',
+    'catgoose/nvim-colorizer.lua',
+    event = 'BufReadPre',
     config = function()
         require('colorizer').setup({
-            rgb_fn = true,
-            hsl_fn = true,
-            css = true,
-            css_fn = true,
+            filetypes = { '*' },
+            buftypes = {},
+            user_commands = true,
+            lazy_load = true,
+
+            user_default_options = {
+                names = true,
+                names_opts = {
+                    lowercase = true,
+                    camelcase = true,
+                },
+
+                RGB = true,
+                RRGGBB = true,
+                RRGGBBAA = true,
+                AARRGGBB = false,
+
+                rgb_fn = true,
+                hsl_fn = true,
+                css = true,
+                css_fn = true,
+
+                tailwind = 'normal',
+                tailwind_opts = {
+                    update_names = false,
+                },
+
+                mode = 'virtualtext',
+                virtualtext = ' ',
+                virtualtext_inline = 'before',
+                virtualtext_mode = 'foreground',
+
+                always_update = false,
+            },
         })
     end,
 }
