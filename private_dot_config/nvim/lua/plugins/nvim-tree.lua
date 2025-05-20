922dc750052c866988e28fe0357224ae9e1259e0
Ahmed <ahmedelsharawy117@gmail.com>
2025-05-20 22:49:19 +0300

    chore: add some options and icones

--- private_dot_config/nvim/lua/plugins/nvim-tree.lua
+++ private_dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -7,22 +7,38 @@ return {
     },
     config = function()
         require('nvim-tree').setup({
-            view = {
-                side = 'right',
+            filters = { dotfiles = false },
+            disable_netrw = true,
+            hijack_cursor = true,
+            sync_root_with_cwd = true,
+            update_focused_file = {
+                enable = true,
+                update_root = false,
             },
-            filters = {
-                dotfiles = false,
+            view = {
+                width = 30,
+                side = 'right',
+                preserve_window_proportions = true,
             },
             git = {
                 enable = false,
                 ignore = true,
             },
-            update_focused_file = {
-                enable = true,
-                update_root = false,
-            },
             renderer = {
                 root_folder_label = false,
+                indent_markers = { enable = true },
+                icons = {
+                    glyphs = {
+                        default = '󰈚',
+                        folder = {
+                            default = '',
+                            empty = '',
+                            empty_open = '',
+                            open = '',
+                            symlink = '',
+                        },
+                    },
+                },
             },
         })
     end,
