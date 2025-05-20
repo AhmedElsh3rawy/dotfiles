80d2359cc9a002cdc382bb8e0be59d63dfbe035b
Ahmed <ahmedelsharawy117@gmail.com>
2025-05-20 22:50:04 +0300

    chore: remove a plugin

--- private_dot_config/nvim/lua/plugins/rust.lua
+++ private_dot_config/nvim/lua/plugins/rust.lua
@@ -7,18 +7,4 @@ return {
             vim.g.rustfmt_autosave = 1
         end,
     },
-    {
-        'saecki/crates.nvim',
-        ft = { 'toml' },
-        config = function(_, opts)
-            local crates = require('crates')
-            crates.setup(opts)
-            require('cmp').setup.buffer({
-                sources = { { name = 'crates' } },
-            })
-            crates.show()
-            vim.keymap.set('n', '<leader>rcu', require('crates').upgrade_all_crates, { desc = 'Update crates' })
-            -- vim.keymap.set("n", "<leader>rcd", ":lua require("crates").open_documentation() <CR>", {})
-        end,
-    },
 }
