34d8744e653e5f0fafcab1ea2a2ea646c244c35c
Ahmed <ahmedelsharawy117@gmail.com>
2025-06-09 01:12:16 +0300

    feat(nvim): catppuccin theme and some overrides

--- private_dot_config/nvim/lua/plugins/theme.lua
+++ private_dot_config/nvim/lua/plugins/theme.lua
@@ -1,13 +1,31 @@
 return {
     {
-        'catppuccin/nvim',
-        name = 'catppuccin',
-        priority = 1000,
-    },
-    {
         'folke/tokyonight.nvim',
         lazy = false,
         priority = 1000,
-        opts = {},
+        config = function()
+            require('tokyonight').setup({
+                style = 'night',
+                on_highlights = function(highlights, _)
+                    highlights['@punctuation.bracket'] = { fg = '#bb9af7' }
+                    highlights['@punctuation.delimiter'] = { fg = '#f7768e' }
+                end,
+            })
+        end,
+    },
+    {
+        'catppuccin/nvim',
+        name = 'catppuccin',
+        priority = 1000,
+        config = function()
+            require('catppuccin').setup({
+                flavour = 'mocha',
+                custom_highlights = {
+                    Normal = { bg = '#1E1D2D' },
+                    ['@punctuation.bracket'] = { fg = '#F38BA8' },
+                    ['@punctuation.delimiter'] = { fg = '#F38BA8' },
+                },
+            })
+        end,
     },
 }
