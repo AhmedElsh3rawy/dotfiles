062a51edface8f5afd7fcf07c270295c3109b0a7
Ahmed <ahmedelsharawy117@gmail.com>
2025-06-09 01:11:25 +0300

    chore(nvim): remvoe plugin

--- private_dot_config/nvim/lua/plugins/mason.lua
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -1,33 +1,14 @@
 return {
-    {
-        'williamboman/mason.nvim',
-        config = function()
-            require('mason').setup({
-                ui = {
-                    icons = {
-                        package_pending = ' ',
-                        package_installed = '󰄳 ',
-                        package_uninstalled = ' 󰚌',
-                    },
+    'mason-org/mason.nvim',
+    config = function()
+        require('mason').setup({
+            ui = {
+                icons = {
+                    package_pending = ' ',
+                    package_installed = '󰄳 ',
+                    package_uninstalled = ' 󰚌',
                 },
-            })
-        end,
-    },
-    {
-        'williamboman/mason-lspconfig.nvim',
-        config = function()
-            require('mason-lspconfig').setup({
-                ensure_installed = {
-                    -- LSPs
-                    'lua_ls',
-                    'rust_analyzer',
-                    'ts_ls',
-                    'pyright',
-                    'clangd',
-                    'gopls',
-                    'bashls',
-                },
-            })
-        end,
-    },
+            },
+        })
+    end,
 }
