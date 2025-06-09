a43b68334513adcfd3dbb852c5fbfe4803543e54
Ahmed <ahmedelsharawy117@gmail.com>
2025-06-10 00:41:23 +0300

    feat(nvim): some tweaks for lualine

--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -7,7 +7,22 @@ return {
             fmt = function(str)
                 return ' ' .. str
             end,
+            color = { gui = 'bold' },
         }
+        local sep = { '%=' }
+        local lsp = {
+            function()
+                local msg = '[LSP]'
+                local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
+                if #buf_clients == 0 then
+                    return msg
+                end
+                return '[' .. buf_clients[1].name .. ']'
+            end,
+            icon = '',
+            color = { fg = '#f7768e' },
+        }
+
         require('lualine').setup({
             options = {
                 globalstatus = true,
@@ -18,7 +33,21 @@ return {
             sections = {
                 lualine_a = { mode },
                 lualine_b = { 'filename' },
-                lualine_c = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = {
+                    'branch',
+                    { 'diff', symbols = { added = ' ', modified = '󰏬 ', removed = ' ' } },
+                    sep,
+                    {
+                        'diagnostics',
+                        symbols = {
+                            error = ' ',
+                            warn = ' ',
+                            hint = ' ',
+                            info = ' ',
+                        },
+                    },
+                    lsp,
+                },
                 lualine_x = { 'encoding', 'fileformat', 'filetype' },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' },
