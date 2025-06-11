068b2219aeac7ee1f34f8ac4abc9eb349716a126
Ahmed <ahmedelsharawy117@gmail.com>
2025-06-11 22:57:42 +0300

    feat(nvim): add new keymap Ctrl+s to save

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -5,20 +5,21 @@ local builtin = require('telescope.builtin')
 
 map('v', 'J', ":m '>+1<CR>gv=gv")
 map('v', 'K', ":m '<-2<CR>gv=gv")
-
+-- Save file
+map('n', '<C-s>', '<cmd>w<CR>')
 map('x', '<leader>p', [["_dP]])
 -- Close active buffer
-map('n', '<leader>x', '<cmd> bd <CR>')
+map('n', '<leader>x', '<cmd>bd<CR>')
 -- Cycle opened buffers
-map('n', '<Tab>', '<cmd> bnext <CR>', opts)
-map('n', '<S-Tab>', '<cmd> bprevious <CR>', opts)
+map('n', '<Tab>', '<cmd>bnext<CR>', opts)
+map('n', '<S-Tab>', '<cmd>bprevious<CR>', opts)
 -- Telescope
 map('n', '<leader>ff', builtin.find_files, { desc = 'Find file' })
 map('n', '<leader>fw', builtin.live_grep, { desc = 'Find word' })
 map('n', '<leader>fh', builtin.help_tags, { desc = 'Find help' })
 map('n', '<leader>fb', builtin.buffers, { desc = 'Find buffer' })
 -- nvim-tree
-map('n', '<leader>e', '<cmd> NvimTreeToggle<CR>', { desc = 'Nvim tree' })
+map('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { desc = 'Nvim tree' })
 -- jump normal mode
 map('i', 'jn', '<Esc>', {})
 -- Navigate vim panes better
