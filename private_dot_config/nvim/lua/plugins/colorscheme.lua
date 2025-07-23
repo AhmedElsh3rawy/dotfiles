366245fcafd195a7bbabb170c05491f3270d6d82
Ahmed <ahmedelsharawy117@gmail.com>
2025-07-24 00:10:45 +0300

    chore: rename

---
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        'folke/tokyonight.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('tokyonight').setup({
+                style = 'night',
+                on_highlights = function(highlights, _)
+                    highlights['@punctuation.bracket'] = { fg = '#bb9af7' }
+                    highlights['@punctuation.delimiter'] = { fg = '#f7768e' }
+                end,
+            })
+        end,
+    },
+    {
+        'sainnhe/gruvbox-material',
+        lazy = false,
+        priority = 1000,
+    },
+}
