bdafb9740839cdd18865033646609767d1a9d082
Ahmed <ahmedelsharawy117@gmail.com>
2025-09-04 22:41:12 +0300

    chore(nvim): make telescope ignore images

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,22 @@ return {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('telescope').setup({
+                defaults = {
+                    file_ignore_patterns = {
+                        '%.png',
+                        '%.jpg',
+                        '%.jpeg',
+                        '%.gif',
+                        '%.svg',
+                        '%.bmp',
+                        '%.webp',
+                        '%.ico',
+                    },
+                },
+            })
+        end,
     },
     {
         'nvim-telescope/telescope-ui-select.nvim',
