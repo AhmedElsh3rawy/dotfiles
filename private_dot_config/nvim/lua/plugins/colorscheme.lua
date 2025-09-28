85c381d1549744a4c2cb560ee72749d3f167a485
Ahmed <ahmedelsharawy117@gmail.com>
2025-09-28 10:13:04 +0300

    feat(nvim): add gurvbox and everforest colorschemes

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -14,4 +14,19 @@ return {
             })
         end,
     },
+    {
+        'sainnhe/gruvbox-material',
+        config = function()
+            vim.g.gruvbox_material_better_performance = 1
+            vim.g.gruvbox_material_background = 'hard'
+        end,
+    },
+    {
+        'sainnhe/everforest',
+        config = function()
+            vim.g.everforest_background = 'hard' -- options: "soft", "medium", "hard"
+            vim.g.everforest_enable_italic = 1 -- enable italics
+            vim.g.everforest_better_performance = 1
+        end,
+    },
 }
