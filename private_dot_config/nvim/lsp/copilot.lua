0441e37dec61710679a96bd8a94dde5c09082a2d
Ahmed <ahmedelsharawy117@gmail.com>
2025-10-01 22:45:47 +0300

    feat(nvim): copilot lsp

---
+++ private_dot_config/nvim/lsp/copilot.lua
@@ -0,0 +1,9 @@
+return {
+    cmd = { 'copilot-language-server', '--stdio' },
+    root_markers = { '.git' },
+    settings = {
+        telemetry = {
+            telemetryLevel = 'all',
+        },
+    },
+}
