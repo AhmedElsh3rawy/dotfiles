a421faa59bddc5e0cca5e35e810e94282c9df119
Ahmed <ahmedelsharawy117@gmail.com>
2025-10-17 16:40:03 +0300

    feat(nvim): add jdtls lsp

---
+++ private_dot_config/nvim/lsp/jdtls.lua
@@ -0,0 +1,8 @@
+return {
+    cmd = { 'jdtls' },
+    filetype = { 'java' },
+    root_markers = {
+        { 'mvnw', 'gradlew', 'build.gradle', 'build.gradle.kts', '.git' },
+        { 'build.xml', 'pom.xml', 'settings.gradle', 'settings.gradle.kts' },
+    },
+}
