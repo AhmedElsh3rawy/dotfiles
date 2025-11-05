4a3f980f2da6f0777da218c81c24120774fe4233
Ahmed <ahmedelsharawy117@gmail.com>
2025-11-06 00:14:11 +0200

    feat(nvim): roslyn.nvim and easy-dotnet.nvim plugins for C#

---
+++ private_dot_config/nvim/lua/plugins/dotnet.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        'seblyng/roslyn.nvim',
+        opts = {},
+    },
+    {
+        'GustavEikaas/easy-dotnet.nvim',
+        config = function()
+            require('easy-dotnet').setup()
+        end,
+    },
+}
