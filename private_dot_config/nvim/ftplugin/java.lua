37266a9b2133457939e1b0af0f9a96610764a43d
Ahmed <ahmedelsharawy117@gmail.com>
2025-11-12 01:40:09 +0200

    chore(nvim): ftplugin for java

---
+++ private_dot_config/nvim/ftplugin/java.lua
@@ -0,0 +1,10 @@
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+local config = {
+    cmd = { vim.fn.expand('~/.local/share/nvim/mason/bin/jdtls') },
+    root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+}
+
+require('jdtls').start_or_attach(config)
