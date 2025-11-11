e70fea18e8664711923d25c76032c5a20e90bf34
Ahmed <ahmedelsharawy117@gmail.com>
2025-11-12 01:36:02 +0200

    chore(nvim): set filetype to cs for dotnet related plugins

--- private_dot_config/nvim/lua/plugins/dotnet.lua
+++ private_dot_config/nvim/lua/plugins/dotnet.lua
@@ -1,10 +1,12 @@
 return {
     {
         'seblyng/roslyn.nvim',
+        ft = 'cs',
         opts = {},
     },
     {
         'GustavEikaas/easy-dotnet.nvim',
+        ft = 'cs',
         config = function()
             require('easy-dotnet').setup()
         end,
