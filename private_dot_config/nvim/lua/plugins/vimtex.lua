25734cd48dd80dd956aaa3fac453bae08481f183
Ahmed <ahmedelsharawy117@gmail.com>
2025-11-12 01:36:56 +0200

    no need

--- private_dot_config/nvim/lua/plugins/vimtex.lua
+++
@@ -1,8 +0,0 @@
-return {
-    -- pacman -S texlive-latex texlive-binextra texlive-latexrecommended texlive-latexextra texlive-fontsrecommended texlive-fontsextra
-    'lervag/vimtex',
-    lazy = false,
-    init = function()
-        vim.g.vimtex_view_method = 'zathura'
-    end,
-}
